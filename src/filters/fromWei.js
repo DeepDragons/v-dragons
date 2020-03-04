6777deb49494963a77cf6e974f4e715ae776787b
Rinat <rinat@Sergeys-Mac-mini.local>
2020-03-04 19:33:00 +0500

    fixed calc

--- src/filters/fromWei.js
+++ src/filters/fromWei.js
@@ -1,22 +1,25 @@
 import { fromWei } from 'web3/lib/utils/utils'
 
-export default function (value, currency) {
+export default function (value, currency, toAdd) {
   let gwei = 900000000000;
   let wei = 100;
 
   if (+value <= wei) {
     value = fromWei(value, 'wei');
-    currency = 'WEI';
+    if (currency !== 'none') {
+      currency = 'WEI';
+    }
   } else if (+value <= gwei) {
     value = fromWei(value, 'gwei');
-    currency = 'GWEI';
-    
+    if (currency !== 'none') {
+      currency = 'GWEI';
+    }
   } else {
     value = fromWei(value, 'ether');
   }
 
-  if (currency) {
-    value = `${value} ${currency}`;
+  if (currency !== 'none') {
+    value = `${value} ${currency }`;
   }
 
   return value;

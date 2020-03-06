a0514658e79a73d5fcecd04041906bc058c2a66e
Rinat <figurative63@protonmail.com>
2020-03-06 09:15:05 +0000

    fixed contracts

--- src/views/FightingGround.vue
+++ src/views/FightingGround.vue
@@ -63,7 +63,7 @@ export default {
   },
   computed: {
     cards() {
-      let tokensForSale = this.sortElements();
+      let tokensForSale = this.sortElements();      
       return this.pageChanged(tokensForSale);
     },
     isNotDragons() {
@@ -75,7 +75,7 @@ export default {
   },
   methods: {
     async preStart() {
-      if (this.cards.length > 0) return null;
+      if (this.cards.length === 0) return null;
       this.loaderShow();
       await this.getDragonsTofight();
       this.loaderHide()
